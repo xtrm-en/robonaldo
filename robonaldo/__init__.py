d2481697cda616f4e05d9bd958c33af9e4015190
xtrm <xtram.dev@gmail.com>
2022-02-28 09:45:09 +0100

    Initial commit

---
+++ robonaldo/__init__.py
@@ -0,0 +1,10 @@
+"""
+Robonaldo - rationator
+"""
+
+__title__ = 'robonaldo'
+__license__ = 'ISC'
+__copyright__ = 'Copyright (c) 2022 xtrm'
+__version__ = '1.0.0'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
